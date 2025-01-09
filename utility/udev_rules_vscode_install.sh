49a71fded2d5f2aa2beb021d8c7a6ff517135f1a
boroscsaba00 <csaba.boros@edu.bme.hu>
2025-01-09 15:20:31 +0100

    adding utility file for vscode and udev rules

---
+++ utility/udev_rules_vscode_install.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+code --version
+
+cd /etc/udev/rules.d/
+sudo touch 99-vesc.rules
+sudo chmod 646 99-vesc.rules
+sudo echo 'KERNEL=="ttyACM[0-9]*", ACTION=="add", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE="0666", GROUP="dialout", SYMLINK+="sensors/vesc"' > 99-vesc.rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo "Reboot your system to activate udev rules"
