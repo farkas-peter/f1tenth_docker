2b736b6d86aabd83941926907148135bc4ce65b4
boroscsaba00 <csaba.boros@edu.bme.hu>
2025-01-20 15:22:42 +0100

    adding access point setting script

---
+++ utility/f1_tenth_wifi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+export CONNECTION_NAME='f1tenth'
+export CONNECTION_SSID='f1tenth'
+export CONNECTION_PASS='f1tenth_2025'
+export CONNECTION_IP4='10.0.0.1/24'
+
+nmcli connection add type wifi ifname wlan0 con-name $CONNECTION_NAME autoconnect yes ssid  $CONNECTION_SSID
+nmcli connection modify $CONNECTION_NAME 802-11-wireless.mode ap 802-11-wireless.band bg
+nmcli connection modify $CONNECTION_NAME wifi-sec.key-mgmt wpa-psk
+nmcli connection modify $CONNECTION_NAME wifi-sec.psk $CONNECTION_PASS
+nmcli connection modify $CONNECTION_NAME ipv4.addresses $CONNECTION_IP4
+nmcli connection modify $CONNECTION_NAME ipv4.method  shared
+nmcli connection up $CONNECTION_NAME
