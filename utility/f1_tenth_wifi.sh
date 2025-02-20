94a085d680a145cca8fa0c0b4db05d10114093b4
farkas-peter <peterfarkas@edu.bme.hu>
2025-02-20 15:36:29 +0100

    chg: connection IP of wifi because wired connection blocked it

--- utility/f1_tenth_wifi.sh
+++ utility/f1_tenth_wifi.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -e
 
+# If cannot ssh from an external machine, maybe an other network blocks it:
+# E.g., the wired connection, if it is in the same IP range (e.g., both starts with 10.0.10)
+# Show networks: nmcli connection show
+# Remove a network: nmcli connection delete <connection_name>
+
 export CONNECTION_NAME='f1tenth'
 export CONNECTION_SSID='f1tenth'
 export CONNECTION_PASS='f1tenth_2025'
-export CONNECTION_IP4='10.0.0.1/24'
+export CONNECTION_IP4='10.0.10.1/24'
 
 nmcli connection add type wifi ifname wlan0 con-name $CONNECTION_NAME autoconnect yes ssid  $CONNECTION_SSID
 nmcli connection modify $CONNECTION_NAME 802-11-wireless.mode ap 802-11-wireless.band bg
